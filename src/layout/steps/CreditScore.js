087b001ac681687f0fdc6eae78d8f0d7ecef288f
Kolya Movchan <kmovchan261@gmail.com>
2024-10-19 15:37:20 +0300

    fix:

--- src/layout/steps/CreditScore.js
+++ src/layout/steps/CreditScore.js
@@ -1,33 +1,31 @@
 import React, { useEffect } from "react";
 import { Card } from "../../components/ItemCard";
 import { Container } from "../container";
-import { utilityBillsList } from "../../mop/utilityBillsList";
+import { creditScoreList } from "../../mop/creditScoreList";
 
-export const UtilityBill = ({ handleUserAnswer, quizData }) => {
+export const CreditScore = ({ handleUserAnswer, quizData }) => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   return (
     <Container className="container-without-cards">
-      <h1 className="title title-master">
-        How much are your monthly electric bills?
-      </h1>
+      <h1 className="title title-master">What is your credit score?</h1>
       <div className="card-container-emojie" style={{ marginBottom: "20px" }}>
-        {utilityBillsList.map((bill) => (
+        {creditScoreList.map((score) => (
           <Card
-            key={bill.id}
-            title={bill.name}
-            img={`/emojies/${bill.id}.png`}
-            onClick={() => handleUserAnswer({ utility_bill_amount: bill.id })}
+            key={score.id}
+            title={score.name}
+            img={`/emojies/${score.id}.png`}
+            onClick={() => handleUserAnswer({ credit_score: score.id })}
             imgHeight={64}
             imgWidth={64}
-            isActive={quizData.utility_bill_amount === bill.id}
-            isDisabled={quizData.hasOwnProperty("utility_bill_amount")}
+            isActive={quizData.credit_score === score.id}
+            isDisabled={quizData.hasOwnProperty("credit_score")}
             classImg="card-emojie"
             style={{
               border: "1px solid #D2D2D2",
-              backgroundColor: "#FAFAFA",
+              backgroundColor: score.color,
             }}
           />
         ))}
--- src/layout/steps/UtilityBill.js
+++ src/layout/steps/UtilityBill.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import { Card } from "../../components/ItemCard";
 import { Container } from "../container";
-import { creditScoreList } from "../../mop/creditScoreList";
+import { utilityBillsList } from "../../mop/utilityBillsList";
+
 export const UtilityBill = ({ handleUserAnswer, quizData }) => {
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -9,22 +10,24 @@ export const UtilityBill = ({ handleUserAnswer, quizData }) => {
 
   return (
     <Container className="container-without-cards">
-      <h1 className="title title-master">What is your credit score?</h1>
+      <h1 className="title title-master">
+        How much are your monthly electric bills?
+      </h1>
       <div className="card-container-emojie" style={{ marginBottom: "20px" }}>
-        {creditScoreList.map((score) => (
+        {utilityBillsList.map((bill) => (
           <Card
-            key={score.id}
-            title={score.name}
-            img={`/emojies/${score.id}.png`}
-            onClick={() => handleUserAnswer({ credit_score: score.id })}
+            key={bill.id}
+            title={bill.name}
+            img={`/emojies/${bill.id}.png`}
+            onClick={() => handleUserAnswer({ utility_bill_amount: bill.id })}
             imgHeight={64}
             imgWidth={64}
-            isActive={quizData.credit_score === score.id}
-            isDisabled={quizData.hasOwnProperty("credit_score")}
+            isActive={quizData.utility_bill_amount === bill.id}
+            isDisabled={quizData.hasOwnProperty("utility_bill_amount")}
             classImg="card-emojie"
             style={{
               border: "1px solid #D2D2D2",
-              backgroundColor: score.color,
+              backgroundColor: "#FAFAFA",
             }}
           />
         ))}

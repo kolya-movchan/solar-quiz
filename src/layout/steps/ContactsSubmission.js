387ff28799a3e3007ae4babe01417f0b74347440
Kolya Movchan <kmovchan261@gmail.com>
2024-10-20 00:03:17 +0300

    feat:

--- src/layout/steps/ContactsSubmission.js
+++ src/layout/steps/ContactsSubmission.js
@@ -278,7 +278,7 @@ export const ContactsSubmission = ({ quizData, onSubmit }) => {
         isLoading={isLoading}
       />
 
-      {!showOTPInput && (
+      {showOTPInput && (
         <OTPModal
           setShowOTPInput={setShowOTPInput}
           formData={formData}

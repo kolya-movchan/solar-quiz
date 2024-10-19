c0c22c22b6683b990a30c1f66764476a8d79b771
Kolya Movchan <kmovchan261@gmail.com>
2024-10-19 15:07:47 +0300

    feat:

---
+++ src/components/IntroDescriptionItem1.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const IntroDescriptionItem = ({ title, description }) => {
+  return (
+    <div style={{ display: "flex", gap: "20px", alignItems: "start" }}>
+      <img src="/icons/triple-lines.svg" alt="" />
+
+      <div
+        style={{
+          display: "flex",
+          alignItems: "start",
+          flexDirection: "column",
+          gap: "8px",
+        }}
+        className="intro-boolet-container"
+      >
+        <h2 style={{ margin: 0 }} className="intro-title-2">
+          {title}
+        </h2>
+        <p style={{ margin: 0, color: "#475467" }}>{description}</p>
+      </div>
+    </div>
+  );
+}; 

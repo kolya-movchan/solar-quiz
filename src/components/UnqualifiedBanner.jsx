868b3372cfec0c23005fc4b6bbb5680beba34a89
Kolya Movchan <kmovchan261@gmail.com>
2024-10-31 19:11:49 +0200

    fix: add tooltip on mobile

--- src/components/UnqualifiedBanner.jsx
+++ src/components/UnqualifiedBanner.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import classNames from "classnames";
 
 export const UnqualifiedBanner = () => {
+  const [showTooltip, setShowTooltip] = useState(false);
+  const bannerRef = useRef(null);
+
+  const handleClick = () => {
+    setShowTooltip(!showTooltip);
+  };
+
+  const handleClickOutside = (event) => {
+    if (bannerRef.current && !bannerRef.current.contains(event.target)) {
+      setShowTooltip(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div
+      ref={bannerRef}
       className="unqualified-banner"
       style={{
         display: "flex",
@@ -10,11 +32,11 @@ export const UnqualifiedBanner = () => {
         padding: "15px",
         boxSizing: "border-box",
         border: "1px solid #B42318",
-        // marginBottom: "0",
         borderRadius: "8px",
         position: "relative",
         cursor: "default",
       }}
+      onClick={handleClick}
     >
       <span style={{ color: "#B42318" }}>Your home status:</span>
       <div style={{ display: "flex", alignItems: "center", gap: "6px" }}>
@@ -26,7 +48,11 @@ export const UnqualifiedBanner = () => {
         />
       </div>
 
-      <div className="tooltip">
+      <div
+        className={classNames("tooltip", {
+          "tooltip-mobile": showTooltip,
+        })}
+      >
         Your home is unqualified based on the home and energy details you
         shared. See below to learn of the different disqualification reasons:
         <ul>

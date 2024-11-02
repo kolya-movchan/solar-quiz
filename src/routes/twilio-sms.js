a7056da3a23e523267b7cd3c499dab6fde79d7d0
Kolya Movchan <kmovchan261@gmail.com>
2024-11-02 23:15:47 +0200

    fix: remove backend

--- src/routes/twilio-sms.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const { sendOTP, verifyOTP } = require("../controller/twilio-sms");
-const router = express.Router();
-
-router.route("/send-otp").post(sendOTP);
-router.route("/verify-otp").post(verifyOTP);
-
-module.exports = router;
